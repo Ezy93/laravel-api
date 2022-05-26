8b6d6151e77ede08041ca779099cc99366de2cc8
Ezy93 <giordani001@gmail.com>
2022-05-26 17:12:23 +0200

    rimozione file app.jss e diviso in front.js e admin.js, modificata la index di category

---
+++ resources/js/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>work in progress</h1>
+        <h2>sito in costruzione</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'root',
+    components: {},
+}
+</script>
+
+<style>
+
+</style>
